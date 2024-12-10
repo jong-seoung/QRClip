b1f71e3fe41e0263c5aec1858600d2f36fac663d
beak jong seoung <jjong015189@gmail.com>
2024-12-10 13:07:15 +0900

    QR 생성시 기존에 존재하면, 기존 객체 리턴

--- qr/views.py
+++ qr/views.py
@@ -1,5 +1,9 @@
+from django.db.models import Q
+
+from rest_framework import status
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from qr.serializers import QRCodeSerializer
 from qr.models import QRCode
@@ -15,3 +19,12 @@ class QRCodeViewSet(MappingViewSetMixin, GenericViewSet, CreateModelMixin):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        content = request.data.get("content")
+
+        existing_qr = QRCode.objects.filter(Q(content=content)).first()
+
+        if existing_qr:
+            serializer = self.get_serializer(existing_qr)
+            return Response(serializer.data, status=status.HTTP_200_OK)
